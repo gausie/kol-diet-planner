68139c9f941735b88e5adcdde6cec6c99c5b95e7
Sam Gaus <sam@gaus.co.uk>
2025-05-09 10:30:27 +0100

    The planner can now basically run in a node/browser environment

--- src/NodePlanner.test.ts
+++ src/NodePlanner.test.ts
@@ -1,26 +1,50 @@
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { NodePlanner } from "./NodePlanner";
+import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
+import * as fs from "fs/promises";
+
+manageFetchMockGlobally();
 
 describe("Planner", async () => {
-  const planner = new NodePlanner({
-    stomach: 15,
-    liver: 14,
-    spleen: 15,
-    valueOfAdventure: 7000,
-    baseMeat: 275,
-    odeToBooze: true,
-    pizzaLover: true,
-    tuxedoShirt: true,
-    saucemaven: true,
-    class: "Pastamancer",
-    sweetSynthesis: true,
-    mayoClinic: true,
-    mafiaPinkyRing: true,
+  beforeAll(async () => {
+    fetchMock
+      .get(
+        "https://pricegun.loathers.net/api/all",
+        200,
+        await fs.readFile(
+          import.meta.dirname + "/__fixtures__/prices.json",
+          "utf-8",
+        ),
+      )
+      .post(
+        "https://data.loathers.net/graphql",
+        200,
+        await fs.readFile(
+          import.meta.dirname + "/__fixtures__/data.json",
+          "utf-8",
+        ),
+      );
   });
 
-  await planner.load();
-
   it("plans", async () => {
+    const planner = new NodePlanner({
+      stomach: 15,
+      liver: 14,
+      spleen: 15,
+      valueOfAdventure: 10000,
+      baseMeat: 275,
+      odeToBooze: true,
+      pizzaLover: true,
+      tuxedoShirt: true,
+      saucemaven: true,
+      class: "Pastamancer",
+      sweetSynthesis: true,
+      mayoClinic: true,
+      mafiaPinkyRing: true,
+    });
+
+    await planner.load();
+
     const plan = planner.plan();
     console.log(plan);
     expect(plan).toMatchObject({});
--- src/NodePlanner.ts
+++ src/NodePlanner.ts
@@ -1,37 +1,98 @@
+import { createClient } from "data-of-loathing";
 import { Planner, PlannerOptions } from "./Planner";
 import * as fs from "node:fs";
 
-type Data = {
-  consumables: {
-    name: string;
-    id: number;
-    price: number;
-    turns: number;
-    stomach: number;
-    liver: number;
-    spleen: number;
-    notes: string;
-    effect: string;
-    effectDuration: number;
-  }[];
-  effects: {
-    id: number;
-    name: string;
-    modifiers: Record<string, string>;
-  }[];
+type ConsumableData = {
+  name: string;
+  id: number;
+  turns: number;
+  range: string[];
+  stomach: number;
+  liver: number;
+  spleen: number;
+  notes: string;
+  effect: string;
+  effectDuration: number;
 };
 
+type EffectData = {
+  id: number;
+  name: string;
+  modifiers: Record<string, string>;
+};
+
+type PriceData = Record<number, number>;
 export class NodePlanner extends Planner {
-  data: Data;
-  prices: Record<number, number> = {};
+  consumables: ConsumableData[] = [];
+  prices: PriceData = {};
+  effects: EffectData[] = [];
 
   constructor(options: PlannerOptions) {
     super(options);
-
-    this.data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
   }
 
-  async load() {
+  async #loadData() {
+    const client = createClient();
+    const result = await client.query({
+      allConsumables: {
+        nodes: {
+          id: true,
+          itemById: {
+            name: true,
+            itemModifierByItem: {
+              modifiers: true,
+            },
+          },
+          adventureRange: true,
+          adventures: true,
+          liver: true,
+          spleen: true,
+          stomach: true,
+          notes: true,
+        },
+      },
+      allEffects: {
+        nodes: {
+          id: true,
+          name: true,
+          effectModifierByEffect: {
+            modifiers: true,
+          },
+        },
+      },
+    });
+
+    this.consumables =
+      result.allConsumables?.nodes
+        ?.filter((r) => r !== null)
+        .map((r) => {
+          const modifiers = r.itemById?.itemModifierByItem?.modifiers ?? {};
+          return {
+            id: r.id,
+            liver: r.liver,
+            name: r.itemById?.name || `[${r.id}]`,
+            spleen: r.spleen,
+            stomach: r.stomach,
+            turns: r.adventures,
+            range:
+              r.adventureRange.match(/(-?\d+)(?:-(-?\d+))?/)?.slice(1) ?? [],
+            notes: r.notes || "",
+            effect: modifiers["Effect"] || "",
+            effectDuration: Number(modifiers["Effect Duration"]) || 0,
+          };
+        }) ?? [];
+
+    this.effects =
+      result.allEffects?.nodes
+        ?.filter((e) => e !== null)
+        .map((e) => ({
+          id: e.id,
+          name: e.name,
+          modifiers: e.effectModifierByEffect?.modifiers || {},
+        })) ?? [];
+  }
+
+  async #loadPrices() {
     const response = await fetch("https://pricegun.loathers.net/api/all");
     const json = (await response.json()) as { value: number; itemId: number }[];
     this.prices = Object.fromEntries(
@@ -39,36 +100,41 @@ export class NodePlanner extends Planner {
     );
   }
 
+  async load() {
+    await this.#loadPrices();
+    await this.#loadData();
+  }
+
   getConsumables(): number[] {
-    return this.data.consumables.map((c) => c.id);
+    return this.consumables.map((c) => c.id);
   }
 
   getStomach(id: number) {
-    return this.data.consumables.find((c) => c.id === id)?.stomach ?? 0;
+    return this.consumables.find((c) => c.id === id)?.stomach ?? 0;
   }
 
   getLiver(id: number) {
-    return this.data.consumables.find((c) => c.id === id)?.liver ?? 0;
+    return this.consumables.find((c) => c.id === id)?.liver ?? 0;
   }
 
   getSpleen(id: number) {
-    return this.data.consumables.find((c) => c.id === id)?.spleen ?? 0;
+    return this.consumables.find((c) => c.id === id)?.spleen ?? 0;
   }
 
   getTurns(id: number) {
-    return this.data.consumables.find((c) => c.id === id)?.turns ?? 0;
+    return this.consumables.find((c) => c.id === id)?.turns ?? 0;
   }
 
   getNotes(id: number): string {
-    return this.data.consumables.find((c) => c.id === id)?.notes ?? "";
+    return this.consumables.find((c) => c.id === id)?.notes ?? "";
   }
 
   getItemEffect(id: number) {
-    return this.data.consumables.find((c) => c.id === id)?.effect ?? "";
+    return this.consumables.find((c) => c.id === id)?.effect ?? "";
   }
 
   getItemEffectDuration(id: number) {
-    return this.data.consumables.find((c) => c.id === id)?.effectDuration ?? 0;
+    return this.consumables.find((c) => c.id === id)?.effectDuration ?? 0;
   }
 
   getEffectModifiers(effect: string) {
@@ -76,9 +142,9 @@ export class NodePlanner extends Planner {
     const match = effectName.match(/\[(\d+)\].*/);
     if (match) {
       const id = Number(match[1]);
-      return this.data.effects.find((e) => e.id === id)?.modifiers;
+      return this.effects.find((e) => e.id === id)?.modifiers;
     }
-    return this.data.effects.find((e) => e.name === effectName)?.modifiers;
+    return this.effects.find((e) => e.name === effectName)?.modifiers;
   }
 
   getPrice(id: number) {
